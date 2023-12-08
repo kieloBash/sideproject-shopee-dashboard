6353e3b6e0c62176e6f54293d80260f367bb255f
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-09 01:08:41 +0800

    trial testing of fetch

--- app/auth/sign-in/user-auth-form.tsx
+++ app/auth/sign-in/user-auth-form.tsx
@@ -24,43 +24,59 @@ export function UserAuthFormLogin({ className, ...props }: UserAuthFormProps) {
   }
 
   const miners = useFetchMiners();
-  console.log(miners);
-
+  
+  if (miners.isLoading)
+    return (
+      <div className="fixed inset-0 z-[100] flex justify-center items-center">
+        <Loader2 className="w-10 h-10 animate-spin" />
+      </div>
+    );
   return (
-    <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
+    <>
+      <ul className="max-h-[10rem] overflow-y-auto">
+        {miners?.data?.map((miner) => {
+          return (
+            <li className="" key={miner.id}>
+              {miner.miner_name}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={cn("grid gap-6", className)} {...props}>
+        <form onSubmit={onSubmit}>
+          <div className="grid gap-2">
+            <div className="grid gap-1">
+              <Label className="sr-only" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                id="email"
+                placeholder="name@example.com"
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                disabled={isLoading}
+              />
+            </div>
+            <div className="grid gap-1">
+              <Label className="sr-only" htmlFor="password">
+                password
+              </Label>
+              <Input
+                id="password"
+                placeholder="password"
+                type="password"
+                disabled={isLoading}
+              />
+            </div>
+            <Button disabled={isLoading}>
+              {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              Login
+            </Button>
           </div>
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="password">
-              password
-            </Label>
-            <Input
-              id="password"
-              placeholder="password"
-              type="password"
-              disabled={isLoading}
-            />
-          </div>
-          <Button disabled={isLoading}>
-            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            Login
-          </Button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
